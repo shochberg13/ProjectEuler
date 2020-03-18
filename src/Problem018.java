1a0b577f894fbf3da3b264e5dbf3f539b73ed893
Seth Hochberg <shochberg13@gmail.com>
2020-03-18 14:14:46 -0400

    Solved problem 18

--- src/Problem018.java
+++ src/Problem018.java
@@ -7,13 +7,27 @@ public class Problem018 {
 	public static void main(String[] args) throws FileNotFoundException {
 		File file = new File("C:\\Users\\Seth\\Desktop\\Java\\Java Text Files\\EP_Problem018.txt");
 		Scanner reader = new Scanner(file);
-
-
-		ArrayList<Integer> numberList = new ArrayList<Integer>(); 
-		while(reader.hasNextLine()){
-			numberList.add(Integer.parseInt(reader.next()));
+		int triSize = 15;
+		
+		int[][] input = new int[triSize][triSize]; 
+		for(int row = 0; row < triSize; row++){
+			for(int col = 0; col <= row; col ++){
+				input[row][col] = Integer.parseInt(reader.next());
+			}
 		}
-		System.out.println(numberList);
-		// Don't know how to make the triangle shape!
+		
+		int maxPath = DFS(input, 0, 0, 0);
+		System.out.println("The maximum path is: " + maxPath);
+		
 	}
+	
+	public static int DFS(int[][]input, int row, int col, int sum){
+		
+		if (row == input.length - 1){
+			return sum + input[row][col];
+		}
+		return Math.max(DFS(input, row + 1, col, sum + input[row][col]), DFS(input, row + 1, col + 1, sum + input[row][col]));
+		
+	}
+	
 }
