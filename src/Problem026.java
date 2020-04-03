bac249757df2f8883748b2ff485cc605f3917b8e
Seth Hochberg <shochberg13@gmail.com>
2020-04-03 15:53:47 -0400

    Problem 26 changed: added functioning manual division method

--- src/Problem026.java
+++ src/Problem026.java
@@ -1,52 +1,47 @@
 public class Problem026 {
-	// Dreadfully incomplete. Might be worth starting from scratch
 	public static void main(String[] args) {
+		for (int i = 2; i < 11; i++ ){
+			System.out.println("1 / " + i + " has a repeating decimal length of " + lengthOfRepeat(i));
+		}
 
-		int max = 10;
-		for (int i = 2; i <= max ; i++){
-			double decimal = (double) 1 / i;
-			System.out.println(i + " " + decimal + " " + isRepeatingDecimal(decimal) + "\tCycleLength: " + cycleLength(decimal));
-		}
 		
 	}
 	
-	/**
-	 * 
-	 * Finds the length of recurring cycles
-	 */
-	public static int cycleLength(double number){
-		String numberString = Double.toString(number);
-		numberString = numberString.substring(2); // Remove the "0" and "."
+	public static String lengthOfRepeat(int divisor){
+		String entireQuotient = "";
+		int dividend = 1;
 		
-		for (int cycleLength = 1; cycleLength < numberString.length() / 2; cycleLength ++ ){
-			boolean correctCycleLength = true;
-			
-			for (int j = numberString.length() - 1; j >= 0; j--){
-				
-				if (numberString.charAt(j) != numberString.charAt(j - cycleLength)){
-					correctCycleLength = false;
-					System.out.println(cycleLength +" "+ j);
-				}
-				
-			}
-			
-			if (correctCycleLength){
-				return cycleLength;
-			}
-		}
-		
-		return 0;
+		do{
+			dividend *= 10;
+			int quotient = dividend / divisor;
+			entireQuotient += quotient;
+			dividend -= divisor * quotient;
+		}while(entireQuotient.length() < 10);
+//		}while(notRepeating(entireQuotient));
+		return entireQuotient;
 	}
 	
-	public static boolean isRepeatingDecimal(double number){
-		String numberString = Double.toString(number);
-		numberString = numberString.substring(2); // Remove the "0" and "."
+	public static boolean notRepeating(String number){
 		
-		if (numberString.length() >= 16){ //EDIT if i create dividing method
+		int length = number.length();
+		if (length < 2){
 			return true;
-		}else{
-			return false;
 		}
 		
+		int lengthOfRepeat = 0;
+		
+		for (int i = 2; i <= length; i++){
+			if (number.charAt(length - 1) == number.charAt(length - i)){
+				break;
+			}else{
+				lengthOfRepeat++;
+			}
+		}
+		
+		
+		
+		
+		return false;
 	}
+	
 }
